764fe195bb61831db502ca7c676a65251bddcab3
Angel Nápoles Nápoles <69412170+angelqva@users.noreply.github.com>
2022-09-14 13:28:48 -0400

    add imagen to text

---
+++ kerasocr.py
@@ -0,0 +1,15 @@
+
+import matplotlib.pyplot as plt
+import cv2
+import keras_ocr
+
+# keras-ocr will automatically download pretrained
+# weights for the detector and recognizer.
+img_texto = cv2.imread("panoramica.jpg")
+cv2.imshow("texto", img_texto)
+cv2.waitKey(0)
+pipeline = keras_ocr.pipeline.Pipeline()
+prediction = pipeline.recognize([img_texto])
+print(prediction)
+keras_ocr.tools.drawAnnotations(plt.imread("panoramica.jpg"), prediction[0])
+plt.show()
---
+++ tesseract.py
@@ -0,0 +1,19 @@
+import pytesseract
+import cv2
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+img_texto = cv2.imread("texto.png")
+imh, imw, _ = img_texto.shape
+cajas_texto = pytesseract.image_to_boxes(
+    img_texto, lang="spa")
+texto_texto = pytesseract.image_to_string(
+    img_texto, lang="spa")
+for caja in cajas_texto.splitlines():
+    b = caja.split(" ")
+    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+    img_texto = cv2.rectangle(img_texto, (x, imh-y),
+                              (w, imh-h), (0, 255, 0), 2)
+
+cv2.imshow("img_texto", img_texto)
+cv2.waitKey(0)
+print(texto_texto)
